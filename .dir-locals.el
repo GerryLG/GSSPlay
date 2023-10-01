466ebbceb5c639bf2e38ad6b6d4ee1798fc7b9eb
GerryLG <40765148+GerryLG@users.noreply.github.com>
2023-10-01 10:18:52 +0100

    Remove irony / cmake-ide based dir-locals and add .cache to gitignore

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil
-  (my-use-irony . t)
-  (my-use-ide . t)
-  (cmake-ide-build-dir . "build")))
-
