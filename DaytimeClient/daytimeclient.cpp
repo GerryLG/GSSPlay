315744ecbdcf6ef1a175066cd5296224488f460e
GerryLG <40765148+GerryLG@users.noreply.github.com>
2023-10-16 14:34:49 +0100

    Pass principal name and optional portnum as arguments.

--- DaytimeClient/daytimeclient.cpp
+++ DaytimeClient/daytimeclient.cpp
@@ -1,33 +1,63 @@
-#include <iostream>
 #include <array>
+#include <charconv>
+#include <cstdlib>
+#include <iostream>
 
 #include <boost/asio.hpp>
+
 #include "gssclientcontext.hpp"
 #include "gsslocalbuffer.hpp"
 #include "gssexception.hpp"
 
 using boost::asio::ip::tcp;
 
+
+void usage() {
+  std::cerr << "Usage: [-p portnum] <hostname> <principal>" << std::endl;
+  std::exit(1);
+}
+
 int main(int argc, char* argv[])
 {
-  try {
-    if (argc != 2) {
-      std::cerr << "Usage: client <host>" << std::endl;
-      return 1;
+  std::string portnum {"1313"};
+
+  int i;
+  for (i = 1; i < argc; ++i) {
+    std::string_view arg {argv[i]};
+    if (arg.empty() || arg.front() != '-')
+      break;
+
+    if (arg == "-p") {
+      if (++i == argc)
+        usage();
+      portnum = argv[i];
+      continue;
     }
 
+    usage();
+  }
+
+  if (argc - i != 2) {
+    usage();
+  }
+
+  auto hostName = std::string(argv[i]);
+  auto principalName = std::string(argv[++i]);
+
+
+  try {
     boost::asio::io_context io_context;
 
     gssxx::GssClientContext context;
     
     tcp::resolver resolver {io_context};
-    tcp::resolver::results_type endpoint_iterator = resolver.resolve(argv[1], "1313");
+    tcp::resolver::results_type endpoint_iterator = resolver.resolve(hostName, portnum);
 
     tcp::socket socket {io_context};
     boost::asio::connect(socket, endpoint_iterator);
 
     try {
-      context.InitiateContext(socket, std::string("sample@") + argv[1]);
+      context.InitiateContext(socket, principalName + "@" + hostName);
     } catch (gssxx::GssException e) {
       std::cerr << "GssException: " << e.what() << std::endl;
       std::cerr << e.message() << std::endl;
--- DaytimeServer/daytimeserver.cpp
+++ DaytimeServer/daytimeserver.cpp
@@ -1,20 +1,51 @@
-#include <iostream>
+#include <charconv>
 #include <cstdlib>
+#include <iostream>
+#include <string_view>
+#include <system_error>
 
 #include <boost/asio.hpp>
 
 #include "tcp_server.hpp"
 
+void usage() {
+  std::cerr << "Usage: [-p portnum] <keytab_file> <principal>" << std::endl;
+  std::exit(1);
+}
+
 int main(int argc, char *argv[])
 {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " <keytab_file>" << std::endl;
-    std::exit(1);
+  unsigned short portnum = 1313;
+
+  int i;
+  for (i = 1; i < argc; ++i) {
+    std::string_view arg {argv[i]};
+    if (arg.empty() || arg.front() != '-')
+      break;
+
+    if (arg == "-p") {
+      if (++i == argc)
+        usage();
+      std::string_view portstr {argv[i]};
+      auto [ptr, ec] = std::from_chars(portstr.cbegin(), portstr.cend(), portnum);
+      if (ec != std::errc() || ptr != portstr.cend())
+        usage();
+      continue;
+    }
+
+    usage();
   }
+
+  if (argc - i != 2) {
+    usage();
+  }
+
+  auto keytabFilename = argv[i];
+  auto principalName = argv[++i];
 
   try {
     boost::asio::io_context io_context;
-    tcp_server server {io_context, 1313, "sample", argv[1]};
+    tcp_server server {io_context, portnum, principalName, keytabFilename};
     io_context.run();
   } catch (std::exception& e) {
     std::cerr << e.what() << std::endl;
